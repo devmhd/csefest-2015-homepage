5199e9232cfb6c631ddb882a81650aaf97b0f641
Mehedee Zaman <devmhd@gmail.com>
2015-04-23 01:53:20 +0600

    Clean up and organize

---
+++ js/home.js
@@ -0,0 +1,56 @@
+var girlRotated = false;
+var spinningGirl;
+function girlSpin(){
+
+	console.log(spinningGirl.className);
+	console.log(girlRotated);
+	if(girlRotated){
+
+		spinningGirl.className ="girl";
+		girlRotated = false;
+
+		setTimeout(girlSpin, 7000);		
+
+	} else {
+
+		spinningGirl.className ="girl rotated";
+		girlRotated = true;
+
+		setTimeout(girlSpin, 1500);
+		
+	}
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	
+	spinningGirl = document.getElementsByClassName('girl');
+	spinningGirl = spinningGirl[0];
+	setTimeout(girlSpin, 500);
+
+});
+
+jQuery(document).ready(function() {
+
+
+			// dynamic scaling
+			var scaleRatio = jQuery(window).width() / 2400.0;
+			console.log("ratio " + scaleRatio);
+			jQuery("body").css({"-webkit-transform":"scale(" + scaleRatio + ")",
+				"-moz-transform":"scale(" + scaleRatio + ")",
+				"-o-transform":"scale(" + scaleRatio + ")",
+				"transform":"scale(" + scaleRatio + ")"});
+
+
+
+			// init flipping countdown
+			var currentDate = new Date();
+			var festDate  = new Date("May 1, 2015 00:00:00"); //Festival day
+			var diff = festDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+			clock = jQuery('.clock').FlipClock(diff, {
+				clockFace: 'DailyCounter',
+				countdown: true
+			});
+
+		});
